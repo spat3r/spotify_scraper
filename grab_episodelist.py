966b85bdb8f4f00acec47d45eafc7641ca13ef1b
spat3r <81962879+spat3r@users.noreply.github.com>
2024-08-28 19:04:26 +0000

    add used libraries for episodelist grabber

--- grab_episodelist.py
+++ grab_episodelist.py
@@ -1,3 +1,10 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from bs4 import BeautifulSoup
+import json
+import re
 
 def grab_episodelist(config_json = "config.txt"):
     options = webdriver.ChromeOptions()
